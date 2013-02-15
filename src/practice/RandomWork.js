f087c5603db8a11b5a3a4c03d770ac1660c4246c
unknown <sharvankumar@.(none)>
2013-02-15 12:40:40 +0530

    First commit just to be familiar with development environment !!

---
+++ src/practice/RandomWork.js
@@ -0,0 +1,14 @@
+/**
+ * @author sharvankumar
+ * @date 15-02-13
+ */
+function ObjectTiero(){
+	this.name = "";
+	this.age;
+}
+
+var person = new ObjectTiero();
+person.name = "SHravan";
+person.age = 25;
+
+print(person.name);
